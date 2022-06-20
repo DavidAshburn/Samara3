a7ee0964f7efbb65ca929712ab3de1afce0766b4
KoyoTree <oneinchwebbing@gmail.com>
2022-06-20 09:21:14 -0700

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+    #@about_me = "My name is Buck Ashburn"
+
+  end
+  
+end
+ 
---
+++ db/migrate/20220619211728_create_clients.rb
@@ -0,0 +1,13 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.string :email
+      t.string :mail_address
+
+      t.timestamps
+    end
+  end
+end
